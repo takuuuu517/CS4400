fb143519f55f008bd2cc932f7b0ad2a6c3795f0d
takuuuu517 <ryoryoryoasa@gmail.com>
2018-10-21 22:45:40 -0600

    in progress

--- HW4/linklab-handout/call.c
+++ HW4/linklab-handout/call.c
@@ -18,7 +18,12 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  path = argv[1];
+  // printf("%s\n", argv[0]);   ./call
+  // printf("%s\n", argv[1]);   .so file
+  // printf("%s\n", argv[2]);   method used in the .so file
+  // printf("%s\n\n", argv[3]); parameter of the method
+
+  path = argv[1]; // ./call
   if (path[0] != '/')
     path = make_relative(path);
   so = dlopen(path, RTLD_NOW | RTLD_GLOBAL);
@@ -26,14 +31,14 @@ int main(int argc, char **argv) {
     fprintf(stderr, "error opening %s\n", path);
     return 1;
   }
-  
-  f = dlsym(so, argv[2]);
+
+  f = dlsym(so, argv[2]); // f = method in the so file
   if (!f) {
     fprintf(stderr, "could not find %s in %s\n", argv[2], argv[1]);
     return 1;
   }
-	    
-  r = f(atoi(argv[3]));
+
+  r = f(atoi(argv[3])); // f = ex. always_ok method
 
   printf("%d\n", r);
 
--- HW4/linklab-handout/enforce.c
+++ HW4/linklab-handout/enforce.c
@@ -14,15 +14,75 @@
    contains the in-memory content of the section */
 #define AT_SEC(ehdr, shdr) ((void *)(ehdr) + (shdr)->sh_offset)
 
+Elf64_Shdr *section_by_index(Elf64_Ehdr *ehdr, int index); // from video
+Elf64_Shdr *section_by_name(Elf64_Ehdr *ehdr, char* name); // from video
+
+
+
 /*************************************************************/
 
-void enforce(Elf64_Ehdr *ehdr) {
-  /* Your work starts here --- but add helper functions that you call
-     from here, instead of trying to put all the work in one
-     function. */
+void enforce(Elf64_Ehdr *ehdr) { // ehdr = output so file　header address {e_shoff, e_shstrndx, etc...}
+  Elf64_Shdr* shdrs = (void*)ehdr + ehdr->e_shoff;
+  char * strs = AT_SEC(ehdr, shdrs + ehdr->e_shstrndx);
+
+  // to print out all section name (.text , .plt, .dynsym)
+  int i;
+  // Elf64_Shdr * test = section_by_name(ehdr, ".gnu.hash");
+
+  for(i = 0 ; i < ehdr->e_shnum; i++)
+  {
+    // strs+shdrs[i].sh_name = .text , .plt, .dynsym
+    // printf("%s\n", strs+shdrs[i].sh_name);
+    printf("%s\n", strs+shdrs[i].sh_name);
+    // printf("%p\n", shdrs+i);
+
+    /* testin for section by index ///////////////////////////////////
+    // Elf64_Shdr * test = section_by_index(ehdr, i);
+    // printf("%s\n", strs+test->sh_name);
+    */////////////////////////////////////////////////////////////////
+
+    // /* testin for section by name ///////////////////////////////////
+    // printf("%p\n", test);
+    // */////////////////////////////////////////////////////////////////
+
+
+
+  }
+
+
+
 }
 
 /*************************************************************/
+
+Elf64_Shdr *section_by_index(Elf64_Ehdr *ehdr, int index)
+{
+  Elf64_Shdr * result ;
+  Elf64_Shdr* shdrs = (void*)ehdr + ehdr->e_shoff;
+
+  result = shdrs+index;
+  return result;
+}
+
+Elf64_Shdr *section_by_name(Elf64_Ehdr *ehdr, char* name)
+{
+
+  Elf64_Shdr * result = NULL;
+  Elf64_Shdr* shdrs = (void*)ehdr + ehdr->e_shoff;
+  char * strs = AT_SEC(ehdr, shdrs + ehdr->e_shstrndx);
+  int i;
+
+  for(i = 0 ; i < ehdr->e_shnum; i++)
+  {
+    if(strcmp(name,strs+shdrs[i].sh_name)==0)
+    {
+      result = shdrs + i;
+    }
+  }
+  return result;
+}
+
+
 
 static void fail(char *reason, int err_code) {
   fprintf(stderr, "%s (%d)\n", reason, err_code);
@@ -38,7 +98,7 @@ static void check_for_shared_object(Elf64_Ehdr *ehdr) {
 
   if (ehdr->e_ident[EI_CLASS] != ELFCLASS64)
     fail("not a 64-bit ELF file", 0);
-  
+
   if (ehdr->e_type != ET_DYN)
     fail("not a shared-object file", 0);
 }
@@ -52,13 +112,21 @@ int main(int argc, char **argv) {
   if (argc != 3)
     fail("expected two file names on the command line", 0);
 
+
+
+  // printf("%s\n",argv[0] ); // ./enforce.
+  // printf("%s\n",argv[1] ); // ex0.so
+  // printf("%s\n",argv[2] ); // dest.so
+
   /* Open the shared-library input file */
-  fd_in = open(argv[1], O_RDONLY);
+  // int open(const char *path, int oflags);
+  fd_in = open(argv[1], O_RDONLY); // Open the file so that it is read only.
   if (fd_in == -1)
     fail("could not open input file", errno);
 
   /* Find out how big the input file is: */
-  len = lseek(fd_in, 0, SEEK_END);
+  // off_t lseek(int fildes, off_t offset, int whence);
+  len = lseek(fd_in, 0, SEEK_END); // 	Offset is to be measured relative to the end of the file.
 
   /* Map the input file into memory: */
   p_in = mmap(NULL, len, PROT_READ, MAP_PRIVATE, fd_in, 0);
@@ -89,7 +157,7 @@ int main(int argc, char **argv) {
 
   /* Close input */
   if (munmap(p_in, len))
-    fail("munmap input failed", errno);    
+    fail("munmap input failed", errno);
   if (close(fd_in))
     fail("close input failed", errno);
 
@@ -98,7 +166,7 @@ int main(int argc, char **argv) {
   enforce(ehdr);
 
   if (munmap(p_out, len))
-    fail("munmap input failed", errno);    
+    fail("munmap input failed", errno);
   if (close(fd_out))
     fail("close input failed", errno);
 
